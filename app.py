a7df3862c6cb8fcbe3130eda6539742a14b9f62f
pawmar-design <pawelmaras08@gmail.com>
2026-05-27 21:08:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ else:
 st.caption(f"Wybrany czas: **{format_wyswietlany}**")
 
 # --- ZAKTUALIZOWANY FEATURE: Filtr klubów ---
-lista_klubow = ["Pura", "Fast", "Padel Park"]
+lista_klubow = ["Pura", "Fast", "Padel Park", "Tennispoint"]
 # Zmiana 1: Dodanie parametru 'placeholder'
 wybrane_kluby = st.multiselect(
     "Wybór klubów", 
